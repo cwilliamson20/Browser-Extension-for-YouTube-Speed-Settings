9e7ba00f0dadb5d18a2983b07e24467a64512d3a
cwilliamson20 <connie.jane.williamson@gmail.com>
2022-11-09 13:50:58 -0800

    added manifest.json file and figured out how to change video speed with javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+// from google this is a way to change the speed of a youtube video:
+// document.getElementsByClassName('html5-main-video')[0].playbackRate = 2.0
+// change 2.0 to the video speed, anwywhere from 0 to 16
+
+document.getElementsByClassName('html5-main-video')[0].playbackRate = 2.0
