f5bf7eebffb59f1f75faa2aeee1c88ae27485292
cwilliamson20 <connie.jane.williamson@gmail.com>
2022-11-20 16:07:42 -0800

    Made extensive changes to try and imitate another speed extension then build from there

--- script.js
+++
@@ -1,5 +0,0 @@
-// from google this is a way to change the speed of a youtube video:
-// document.getElementsByClassName('html5-main-video')[0].playbackRate = 2.0
-// change 2.0 to the video speed, anwywhere from 0 to 16
-
-document.getElementsByClassName('html5-main-video')[0].playbackRate = 2.0
